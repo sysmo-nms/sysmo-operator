779c67649d18f8f6ab081ab18842f183ab7cd7ba
ssbx <sserre.bx@gmail.com>
2013-06-02 23:13:17 +0200

    asn tests

--- test/asn1/asnTest.py
+++ test/asn1/asnTest.py
@@ -1,7 +1,32 @@
 #!/usr/bin/env python2
 
-import pyasn1
-
+from pyasn1.type import univ, namedtype
+from pyasn1.codec.ber import encoder, decoder
 
 " decode and encode asn1 terms "
-print "hello pyasn1"
+
+
+
+" encode "
+class CommandResponce(univ.Choice):
+    componentType = namedtype.NamedTypes(
+        namedtype.NamedType('aab', univ.Integer()),
+        namedtype.NamedType('bbc', univ.OctetString())
+    )
+
+c = CommandResponce()
+c.setComponentByName('aab', 123)
+c.setComponentByName('bbc', 'hello jojo')
+print "initial value!!!!!!!!!!!!!!! ", c
+
+binPdu = encoder.encode(c)
+print "value encoded!!!!!!!!!!!!!!! ", binPdu
+
+test = decoder.decode(binPdu)
+print "value decoded!!!!!!!!!!!!!!! ", test
+
+
+" decode "
+fd  = open("pdu.bin")
+pdu = fd.read()
+print "hello pyasn1", pdu
