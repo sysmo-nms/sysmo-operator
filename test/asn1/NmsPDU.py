98a7f870919f8fd3950fa3bc4c4877a3fe7c3ffa
ssbx <sserre.bx@gmail.com>
2013-06-04 21:25:12 +0200

    asn work

--- test/asn1/NmsPDU.py
+++ test/asn1/NmsPDU.py
@@ -4,13 +4,13 @@ from pyasn1.type import char, univ, namedtype, tag
 from pyasn1.codec.ber import encoder, decoder
 
 class NmsPDU_TrackerPDU_FromServer_CmdResp(univ.Sequence):
-    compenentType = namedtype.NamedTypes(
+    componentType = namedtype.NamedTypes(
         namedtype.NamedType('cmdId', univ.Integer()),
         namedtype.NamedType('cmdMsg', char.PrintableString())
     )
 
 class NmsPDU_TrackerPDU_FromServer(univ.Choice):
-    compenentType = namedtype.NamedTypes(
+    componentType = namedtype.NamedTypes(
         namedtype.NamedType('targetInfo', univ.Integer()),
         namedtype.NamedType('probeInfo', univ.Integer()),
         namedtype.NamedType('probeFetch', univ.Integer()),
@@ -20,7 +20,7 @@ class NmsPDU_TrackerPDU_FromServer(univ.Choice):
     )
 
 class NmsPDU_TrackerPDU(univ.Choice):
-    compenentType = namedtype.NamedTypes(
+    componentType = namedtype.NamedTypes(
         namedtype.NamedType('fromServer', NmsPDU_TrackerPDU_FromServer()),
         namedtype.NamedType('fromClient', univ.Integer()),
     )
@@ -32,4 +32,24 @@ class NmsPDU(univ.Choice):
         namedtype.NamedType('modTrackerPDU', NmsPDU_TrackerPDU())
     )
 
-print "hello"
+a = NmsPDU()
+b = NmsPDU_TrackerPDU()
+c = NmsPDU_TrackerPDU_FromServer()
+d = NmsPDU_TrackerPDU_FromServer_CmdResp()
+
+d.setComponentByName('cmdId', 34)
+d.setComponentByName('cmdMsg', "hello")
+
+c.setComponentByName('cmdResp', d)
+b.setComponentByName('fromServer', c)
+a.setComponentByName('modTrackerPDU', b)
+
+fd  = open("NmsPDU_modTrackerPDU_fromServer_cmdResp_CmdResponce.bin")
+modTrackerPdu = fd.read()
+
+x = encoder.encode(d)
+print x
+w, y = decoder.decode(x, asn1Spec=NmsPDU_TrakcerPDU_FromServer_CmdResp())
+print y 
+
+# VOIR PYASN1 Example Modules
