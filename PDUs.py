eed1296093af24c7afa3313abb6b0ff0ce23f78b
ssbx <sserre.bx@gmail.com>
2013-06-07 20:45:15 +0200

    asn work

--- PDUs.py
+++ PDUs.py
@@ -4,6 +4,23 @@ from pyasn1.type import char,univ,namedtype,tag,namedval
 from pyasn1.codec.ber import encoder,decoder
 
 " Supercast PDUs are defined here "
+class SupercastChan(char.PrintableString): pass
+
+class SupercastGroup(char.PrintableString): pass
+
+class SupercastChanInfoType(univ.Enumerated):
+    namedValues = namedval.NamedValues(
+        ('create', 0),
+        ('delete', 1),
+        ('update', 2)
+    )
+
+class SupercastChanInfo(univ.Sequence):
+    componentType = namedtype.NamedTypes(
+        namedtype.NamedType('channel', SupercastChan()),
+        namedtype.NamedType('type', SupercastChanInfoType())
+    )
+    
 class SupercastPDU_fromServer_authReq(univ.Enumerated):
     namedValues = namedval.NamedValues(
         ('localfile', 0),
@@ -25,6 +42,13 @@ class SupercastPDU_fromServer_authError_error(univ.Enumerated):
     )
 
 class SupercastPDU_fromServer_authError(univ.Sequence):
+    #tagSet = univ.Enumerated.tagSet.tagExplicitly(
+    #    tag.Tag(
+    #        tag.tagClassContext,
+    #        tag.tagFormatSimple,
+    #        3
+    #    )
+    #),
     componentType = namedtype.NamedTypes(
         namedtype.NamedType('error', SupercastPDU_fromServer_authError_error()),
         namedtype.NamedType('userId',  char.PrintableString()),
@@ -33,10 +57,46 @@ class SupercastPDU_fromServer_authError(univ.Sequence):
 
 class SupercastPDU_fromServer(univ.Choice):
     componentType = namedtype.NamedTypes(
-        namedtype.NamedType('chanInfo', char.PrintableString()),
-        namedtype.NamedType('authReq', SupercastPDU_fromServer_authReq()),
-        namedtype.NamedType('authAck', SupercastPDU_fromServer_authAck()),
-        namedtype.NamedType('authError', SupercastPDU_fromServer_authError())
+        namedtype.NamedType(
+            'chanInfo',
+            char.PrintableString().tagSet.tagExplicitly(
+                tag.Tag(
+                    tag.tagClassContext,
+                    tag.tagFormatSimple,
+                    0
+                )
+            )
+        ),
+        namedtype.NamedType(
+            'authReq',
+            SupercastPDU_fromServer_authReq().tagSet.tagExplicitly(
+                tag.Tag(
+                    tag.tagClassContext,
+                    tag.tagFormatSimple,
+                    1
+                )
+            )
+        ),
+        namedtype.NamedType(
+            'authAck',
+            SupercastPDU_fromServer_authAck().tagSet.tagExplicitly(
+                tag.Tag(
+                    tag.tagClassContext,
+                    tag.tagFormatSimple,
+                    2
+                )
+            )
+        ),
+        namedtype.NamedType(
+            'authError', 
+            SupercastPDU_fromServer_authError().tagSet.tagExplicitly(
+                tag.Tag(
+                    tag.tagClassContext,
+                    tag.tagFormatSimple,
+                    3
+                )
+            )
+        )
     )
 
 class SupercastPDU_fromClient(univ.Choice):
@@ -48,7 +108,8 @@ class SupercastPDU_fromClient(univ.Choice):
 class SupercastPDU(univ.Choice):
     componentType = namedtype.NamedTypes(
         namedtype.NamedType('fromServer',SupercastPDU_fromServer()),
-        namedtype.NamedType('fromClient', SupercastPDU_fromClient())
+        namedtype.NamedType('fromClient', SupercastPDU_fromClient()),
+        namedtype.NamedType('testouille', char.PrintableString())
     )
 
 
@@ -71,8 +132,43 @@ class TrackerPDU(univ.Choice):
 class NmsPDU(univ.Choice):
     componentType = namedtype.NamedTypes(
         namedtype.NamedType('modSupercastPDU', SupercastPDU()),
-        namedtype.NamedType('modEsnmpPDU'    , EsnmpPDU()),
+        namedtype.NamedType('modEsnmpPDU'    , char.PrintableString()),
         namedtype.NamedType('modTrackerPDU'  , TrackerPDU())
+    )
+
+class TestPDU(univ.Choice):
+    componentType = namedtype.NamedTypes(
+        #namedtype.NamedType('fromServer',SupercastPDU_fromServer()),
+        namedtype.NamedType(
+            'fromClient',
+            SupercastPDU_fromClient().subtype(
+                implicitTag=tag.Tag(
+                    tag.tagClassContext,
+                    tag.tagFormatSimple,
+                    0
+                )
+            )
+        ),
+        namedtype.NamedType(
+            'testouille1',
+            char.PrintableString().subtype(
+                implicitTag=tag.Tag(
+                    tag.tagClassContext,
+                    tag.tagFormatSimple,
+                    1
+                )
+            )
+        ),
+        namedtype.NamedType(
+            'testouille',  
+            char.PrintableString().subtype(
+                implicitTag=tag.Tag(
+                    tag.tagClassContext,
+                    tag.tagFormatSimple,
+                    2
+                )
+            )
+        )
     )
 
 if __name__ == "__main__":
@@ -86,15 +182,20 @@ if __name__ == "__main__":
     xPdu = SupercastPDU_fromServer()
     xPdu.setComponentByName('authError', wPdu)
     
-    yPdu = SupercastPDU()
-    yPdu.setComponentByName('fromServer', xPdu)
+    #yPdu = TestPDU()
+    #yPdu.setComponentByName('testouille', "hello pdu")
     
-    zPdu = NmsPDU()
-    zPdu.setComponentByName('modSupercastPDU', yPdu)
-    print zPdu.prettyPrint()
+    #zPdu = NmsPDU()
+    #zPdu.setComponentByName('modSupercastPDU', yPdu)
+    #print zPdu.prettyPrint()
     
-    #encodeTest = xPdu
-    #pdu = encoder.encode(encodeTest)
-    #print encodeTest.prettyPrint, pdu
-    #pduDec, other = decoder.decode(pdu, asn1Spec=encodeTest)
-    
+    encodeTest = wPdu
+
+    pdu = encoder.encode(encodeTest)
+    print "encode: ", encodeTest.prettyPrint()
+    print "len of pdu:-------> ", len(pdu)
+    print "------->", pdu, "<------"
+    # XXX Error here
+    pduDec, other = decoder.decode(pdu, asn1Spec=encodeTest)
+    print "ret -> ", pduDec.prettyPrint(), other
+    # XXX Error here
