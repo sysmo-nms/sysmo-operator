fd9b73d4af4e372906a78dfdf9f9432df73403ac
ssbx <sserre.bx@gmail.com>
2013-06-19 13:44:32 +0200

    left list ui work

--- tkorder/ModTracker.py
+++ tkorder/ModTracker.py
@@ -21,7 +21,16 @@ class ModTracker(QtGui.QSplitter):
         self.addWidget(RightPane(self))
 
     def handleMsg(self, msg):
-        print "modTracker message is ", msg
+        mType = msg['msgType']
+
+        if   (mType == 'probeInfo'):
+            TrackerTViewModel.handleProbeInfo(msg)
+        elif (mType == 'targetInfo'):
+            TrackerTViewModel.handleTargetInfo(msg)
+        elif (mType == 'probeModInfo'):
+            TrackerTViewModel.handleProbeModInfo(msg)
+        else:
+            print "unknown message type: ", mType
 
 
 class LeftPane(QtGui.QFrame):
@@ -44,7 +53,7 @@ class LeftPane(QtGui.QFrame):
     def __init__(self, parent):
         super(LeftPane, self).__init__(parent)
         LeftPane.setSingleton(self)
-        self.treeview   =  LeftPaneTree(self)
+        self.treeview   =  TrackerTView(self)
         self.button     =  QtGui.QPushButton("left here", self)
         grid = QtGui.QGridLayout()
         grid.addWidget(self.button,   0, 0)
@@ -52,16 +61,8 @@ class LeftPane(QtGui.QFrame):
         self.setLayout(grid)
 
 
-class LeftPaneTree(QtGui.QTreeView):
-    def __init__(self, parent):
-        super(LeftPaneTree, self).__init__(parent)
-        model = QtGui.QFileSystemModel()
-        model.setRootPath(QtCore.QDir.currentPath())
-        self.setModel(model)
-
 
 class RightPane(QtGui.QFrame):
-    # TODO stacked widget
     def __init__(self, parent):
         super(RightPane, self).__init__(parent)
         self.button      =  QtGui.QPushButton("toggle left")
@@ -73,3 +74,77 @@ class RightPane(QtGui.QFrame):
             QtCore.SIGNAL("clicked()"),
             LeftPane.toggle
         )
+
+
+#####################################################################
+#####################################################################
+###### TREEVIEW MODEL ###############################################
+#####################################################################
+#####################################################################
+class TrackerTView(QtGui.QTreeView):
+    def __init__(self, parent):
+        super(TrackerTView, self).__init__(parent)
+        self.setModel(TrackerTViewModel())
+
+
+class TrackerTViewModel(QtGui.QStandardItemModel):
+    @classmethod
+    def setTVM(cls, i):
+        cls.tvm  = i
+
+    @classmethod
+    def handleTargetInfo(cls, msg):
+        cls.tvm.tInfo(msg)
+
+    @classmethod
+    def handleProbeInfo(cls, msg):
+        cls.tvm.pInfo(msg)
+
+    @classmethod
+    def handleProbeModInfo(cls, msg):
+        cls.tvm.pModInfo(msg)
+
+    def __init__(self):
+        super(TrackerTViewModel, self).__init__()
+        parentItem  = self.invisibleRootItem()
+        self.elements    = QtGui.QStandardItem("Elements")
+        self.views       = QtGui.QStandardItem("Views")
+        self.probes      = QtGui.QStandardItem("Probes")
+        parentItem.appendRows([self.elements,self.views,self.probes])
+        TrackerTViewModel.setTVM(self)
+
+    def pModInfo(self, msg):
+        val     = msg['value']
+        info    = val['info']
+        name    = val['name']
+        #probesRoot = self.findItems(
+            #"Probes", 
+            #flags=QtCore.Qt.MatchExactly,
+            #column=0
+        #)
+        self.probes.appendRow(QtGui.QStandardItem(name))
+
+    def tInfo(self, msg):
+        val     = msg['value']
+        channel = val['channel']
+        infoType = val['infoType']
+        if infoType == 'create':
+            self.elements.appendRow(QtGui.QStandardItem(channel))
+            self.elements.sortChildren(0)
+
+    def pInfo(self, msg):
+        val     = msg['value']
+        parent  = val['channel']
+        name    = val['name']
+        probeId = val['id']
+
+        parentItemList = self.findItems(
+            parent,
+            flags=QtCore.Qt.MatchRecursive,
+            column=0
+        )
+        parentItem = parentItemList.pop()
+        parentItem.appendRow(QtGui.QStandardItem(name))
+        print val
+
+
