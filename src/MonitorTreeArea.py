3fdb9a390b980144ad407fd0013632f479a2e344
ssbx <sserre.bx@gmail.com>
2013-11-28 20:58:11 +0100

    treeview is ok

--- src/MonitorTreeArea.py
+++ src/MonitorTreeArea.py
@@ -18,7 +18,7 @@ class TreeContainer(QFrame):
         TreeContainer.singleton = self
         self.trackerMain  = parent
 
-        self.treeview   = MonitorTView(self)
+        self.treeview   = MonitorTreeView(self)
         self.searchBar  = MonitorTreeSearch(self)
         self.info       = MonitorTreeAreaInfo(self)
 
@@ -98,122 +98,319 @@ class MonitorTreeAreaInfo(QTextEdit):
         self.append(str(msg))
 
 
-###############################################################################
-#### PROTOTYPE POUR TREEVIEW ##################################################
-###############################################################################
-class TreeNode(object):
-    def __init__(self, parent, row):
-        self.parent = parent
-        self.row = row
-        self.subnodes = self._getChildren()
-
-    def _getChildren(self):
-        raise NotImplementedError()        
-
-# Nous allons creer une classe mere de nos futurs modeles
-# pour limiter la complexite et gerer ce que l'on peut gerer en amont :
-class TreeModel(QAbstractItemModel):
-    def __init__(self):
-        QAbstractItemModel.__init__(self)
-        self.rootNodes = self._getRootNodes()
-
-    # a implementer par la future classe fille
-    def _getRootNodes(self):
-        raise NotImplementedError()
-    
-    # cette methode heritee de QAbstractItemModel doit retourner
-    # l'indice de l'enregistrement en entree moyennant le parent (un QModelIndex)
-    # c.f. paragraph suivant pour plus d'explications.
-    def index(self, row, column, parent):
-        # si l'indice du parent est invalide
-        if not parent.isValid():
-            return self.createIndex(row, column, self.rootNodes[row])
-        parentNode = parent.internalPointer()
-        return self.createIndex(row, column, parentNode.subnodes[row])
-
-    # cette methode heritee de QAbstractItemModel doit retourner
-    # l'indice du parent de l'indice donne en parametre
-    # ou un indice invalide (QModelIndex()) si le noeud n'a pas de parent
-    # ou si la requete est incorrecte
-    # c.f. paragraph suivant pour plus d'explications.
-    def parent(self, index):
-        if not index.isValid():
-            return QModelIndex()
-        # on recupere l'objet sous-jacent avec la methode internalPointer de l'indice
-        node = index.internalPointer()
-        if node.parent is None:
-            return QModelIndex()
-        else:
-            # si tout est valide alors on cree l'indice associe pointant vers le parent
-            return self.createIndex(node.parent.row, 0, node.parent)
-
-    def reset(self):
-        self.rootNodes = self._getRootNodes()
-        QAbstractItemModel.reset(self)
-
-    def rowCount(self, parent):
-        if not parent.isValid():
-            return len(self.rootNodes)
-        node = parent.internalPointer()
-        return len(node.subnodes)
-
-
 
 ##############################################################################
 ### TREEVIEW #################################################################
 ##############################################################################
-class MonitorTView(QTreeView):
+class MonitorTreeView(QTreeView):
     def __init__(self, parent):
-        super(MonitorTView, self).__init__(parent)
-        model = NamesModel()
-        self.setModel(model)
+        super(MonitorTreeView, self).__init__(parent)
+        std     = MonitorTreeModel(self)
+        self.setModel(std)
         self.setSelectionMode(QAbstractItemView.ExtendedSelection)
-        self.setModel(model)
-            
-class NamedElement(object): # notre structure interne pour gerer les objets
-    def __init__(self, name, subelements):
-        self.name = name
-        self.subelements = subelements
+        self.setIconSize(QSize(25, 25)) 
+        self.setHeaderHidden(True)
+        self.setAlternatingRowColors(True)
 
-# notre noeud concret implementant getChildren
-class NamedNode(TreeNode):
-    def __init__(self, ref, parent, row):
-        self.ref = ref
-        TreeNode.__init__(self, parent, row)
+class MonitorTreeModel(QStandardItemModel):
+    def __init__(self, parent):
+        super(MonitorTreeModel, self).__init__(parent)
+        sigDict = ChannelHandler.singleton.masterSignalsDict
+        sigDict['targetInfo'].signal.connect(self._handleTargetInfo)
+        sigDict['probeInfo'].signal.connect(self._handleProbeInfo)
 
-    # renvoie la liste des noeuds fils en utilisant la liste subelements de 
-    # notre objet (interne) NamedElement
-    def _getChildren(self):
-        return [NamedNode(elem, self, index)
-            for index, elem in enumerate(self.ref.subelements)]
-        
-# et enfin notre modele avec 
-class NamesModel(TreeModel):
-    def __init__(self, rootElements):
-        self.rootElements = rootElements
-        TreeModel.__init__(self)
+    def _handleTargetInfo(self, msg):
+        target = self._itemExist(msg['value']['name'])
+        if self._itemExist(msg['value']['name']) == []:
+            self.appendRow(TargetItem(msg))
+        else:
+            self._updateRow(target.pop(), msg)
 
-    def _getRootNodes(self):
-        return [NamedNode(elem, None, index)
-            for index, elem in enumerate(self.rootElements)]
+    def _handleProbeInfo(self, msg):
+        target = msg['value']['target']
+        parent = self._itemExist(target)
+        if parent == []:
+            self.appendRow(ProbeItem(msg))
+        else:
+            targetItem = parent.pop()
+            targetItem.handleProbeInfo(msg)
 
-    def columnCount(self, parent):
-        return 1
+    def _updateRow(self, item, msg):
+        item.updateState(msg)
 
-    # permet de recuperer les donnees liees a un indice et un role.
-    # ces donnees peuvent ainsi varier selon le role.
-    def data(self, index, role):
-        if not index.isValid():
+    def _itemExist(self, itemName):
+        itemList = self.findItems(
+            itemName,
+            flags  = Qt.MatchRecursive,
+            column = 0
+        )
+        return itemList
+
+class TargetItem(QStandardItem):
+    def __init__(self, data):
+        super(TargetItem, self).__init__()
+        self.name       = data['value']['name']
+        self.status     = 'UNKNOWN'
+        self.targetDict = data
+        self.setFlags(Qt.ItemIsSelectable|Qt.ItemIsEnabled)
+
+    def data(self, role):
+        if   role == Qt.DecorationRole:
+            return self._getIconStatus()
+        elif role == Qt.DisplayRole:
+            return self.name
+        else:
+            return QStandardItem.data(self, role)
+
+    def handleProbeInfo(self, msg):
+        count   = self.rowCount()
+        probe   = msg['value']['name']
+        probeExist = False
+        for i in range(count):
+            if self.child(i).name == probe:
+                probeExist  = True
+                child       = self.child(i)
+                break
+
+        if probeExist == False:
+            self.appendRow(ProbeItem(msg))
+        else:
+            child.updateState(msg)
+        self._setWorstStatus()
+        self.emitDataChanged()
+
+    def updateState(self, msg):
+        self.targetDict = msg
+
+    def __lt__(self, other): pass
+
+    def _getIconStatus(self):
+        if self.status  == 'UNKNOWN':
+            return TkorderIcons.get('weather-clear-night')
+        elif self.status == 'WARNING':
+            return TkorderIcons.get('weather-showers')
+        elif self.status == 'CRITICAL':
+            return TkorderIcons.get('weather-severe-alert')
+        elif self.status == 'OK':
+            return TkorderIcons.get('weather-clear')
+
+    def _setWorstStatus(self):
+        count = self.rowCount()
+        status = list()
+        for i in range(count):
+            probe = self.child(i)
+            status.append(probe.status)
+
+        if 'CRITICAL' in status:
+            self.status = 'CRITICAL'
+            return
+        elif 'WARNING' in status:
+            self.status = 'WARNING'
+            return
+        elif 'UNKNOWN' in status:
+            self.status = 'UNKNOWN'
+            return
+        elif 'OK' in status:
+            self.status = 'OK'
+            return
+
+
+class ProbeItem(QStandardItem):
+    def __init__(self, data):
+        super(ProbeItem, self).__init__()
+        self.name   = data['value']['name']
+        self.target = data['value']['target']
+        self.status = data['value']['status']
+        self.probeDict = data
+        print self.flags()
+        self.setFlags(Qt.ItemIsSelectable|Qt.ItemIsEnabled)
+
+    def data(self, role):
+        if   role == Qt.DecorationRole:
+            return self._getIconStatus()
+        elif role == Qt.DisplayRole:
+            return self.name
+        elif role == Qt.EditRole:
             return None
-        node = index.internalPointer()
-        if role == Qt.DisplayRole and index.column() == 0:
-            return node.ref.name
-        return None
+        else:
+            return QStandardItem.data(self, role)
 
-    def headerData(self, section, orientation, role):
-        if orientation == Qt.Horizontal and role == Qt.DisplayRole \
-            and section == 0:
-            return 'Name'
-        return None
-            
+    def updateState(self, data):
+        self.status     = data['value']['status']
+        self.probeDict  = data
+        self.emitDataChanged()
 
+    def __lt__(self, other): pass
+
+    def _getIconStatus(self):
+        if self.status == 'UNKNOWN':
+            return TkorderIcons.get('weather-clear-night')
+        if self.status == 'WARNING':
+            return TkorderIcons.get('weather-showers')
+        if self.status == 'CRITICAL':
+            return TkorderIcons.get('weather-severe-alert')
+        if self.status == 'OK':
+            return TkorderIcons.get('weather-clear')
+
+
+
+# class MonitorTreeView(QTreeView):
+#     def __init__(self, parent):
+#         super(MonitorTreeView, self).__init__(parent)
+#         # MonitorTreeView.singleton = self
+#         targetModel = MonitorTreeViewModel(self)
+#         self.setSelectionMode(QAbstractItemView.ExtendedSelection)
+#         self.setModel(targetModel)
+# 
+#         #self.setHeaderHidden(True)
+#         #self.setAnimated(True)
+#         #self.setIndentation(15)
+#         #self.setUniformRowHeights(True)
+# 
+#         # <- QAbasctractItemView 
+#         #self.setAlternatingRowColors(True)
+#         #self.setDragEnabled(False)
+#         #self.setVerticalScrollMode(QAbstractItemView.ScrollPerPixel)
+#         #self.setHorizontalScrollMode(QAbstractItemView.ScrollPerPixel)
+#         #self.setIconSize(QSize(25, 25)) 
+#         #self.setSelectionBehavior(QAbstractItemView.SelectItems)
+# 
+#         # <- QAbstractScrollArea
+#         #self.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+#         #self.setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+# 
+#         # from QTreeView
+#         #self.expandAll()
+# 
+#         # slots
+#         # self.selectionChanged.connect(self._userSelection)
+# 
+#     def selectionChanged(self, selected, deselected):
+#         print selected, deselected
+#         QTreeView.selectionChanged(self, selected, deselected)
+# 
+#     #def userClic(self, index):
+#         #item    = self.targetModel.itemFromIndex(index)
+#         #parent  = item.parent()
+#         #emitDict = dict()
+# 
+#         #if isinstance(parent, QStandardItem):
+#             #emitDict['target']  = parent.data(Qt.DisplayRole)
+#             #emitDict['probeId'] = item.data(Qt.DisplayRole)
+#         #else:
+#             #emitDict['target']  = item.data(Qt.DisplayRole)
+#             #emitDict['probeId'] = None
+#         #MonitorEvents.singleton.treeviewClicked.emit(emitDict)
+# 
+# 
+# class MonitorTreeViewModel(QStandardItemModel):
+#     def __init__(self, parent):
+#         super(MonitorTreeViewModel, self).__init__(parent)
+#         #MonitorTreeViewModel.singleton = self
+#         #parentItem = self.invisibleRootItem()
+# 
+#         # QStandardItemModel
+#         self.setColumnCount(1)
+#         self.setHorizontalHeaderLabels(['Targets/Probes'])
+# 
+#         # signals to receive
+#         sigDict = ChannelHandler.singleton.masterSignalsDict
+#         sigDict['probeInfo'].signal.connect(self._handleProbeInfo)
+#         sigDict['targetInfo'].signal.connect(self._handleTargetInfo)
+#         sigDict['probeModInfo'].signal.connect(self._handleProbeModInfo)
+# 
+#     def findTargetByName(self, targetName):
+#         parentItemList = self.findItems(
+#             targetName,
+#             flags=Qt.MatchRecursive,
+#             column=0
+#         )
+#         parentItem = parentItemList.pop()
+#         return parentItem
+# 
+#     def _handleProbeModInfo(self, msg): pass
+# 
+#     def _handleTargetInfo(self, msg):
+#         val         = msg['value']
+#         name        = val['name']
+#         infoType    = val['infoType']
+#         properties  = val['properties']
+#         itemType    = 'target'
+#         icon        = TkorderIcons.get('weather-clear-night')
+#         #i2  = icon.pixmap(5,5)
+#         if infoType == 'create':
+#             newItem = QStandardItem()
+#             newItem.setEnabled(True)
+#             print newItem.flags()
+#             newItem.setFlags(Qt.ItemIsSelectable)
+#             newItem.setData(name,    Qt.DisplayRole)
+#             newItem.setData(icon,       Qt.DecorationRole)
+#             newItem.setData(itemType,   Qt.UserRole)
+#             newItem.setData(name,    Qt.UserRole + 1)
+#             newItem.setData(properties, Qt.UserRole + 2)
+#             newItem.setFlags(Qt.ItemIsSelectable)
+#             newItem.setFlags(Qt.ItemIsEnabled)
+#             self.appendRow(newItem)
+#             #self.elements.sortChildren(0)
+# 
+#     def _handleProbeInfo(self, msg):
+#         val         = msg['value']
+#         parent      = val['target']
+#         name        = val['name']
+#         probeId     = val['id']
+#         status      = val['status']
+#         timeout     = val['timeout']
+#         step        = val['step']
+#         pid         = val['id']
+#         itemType    = 'probe'
+#         if (status == 'OK'):
+#             icon = TkorderIcons.get('weather-clear')
+#         elif (status == 'WARNING'):
+#             icon = TkorderIcons.get('weather-showers')
+#         elif (status == 'CRITICAL'):
+#             icon = TkorderIcons.get('weather-severe-alert')
+#         elif (status == 'UNKNOWN'):
+#             icon = TkorderIcons.get('weather-few-clouds-night')
+#         else:
+#             print "status is ", status
+#             icon        = TkorderIcons.get('weather-clear-night')
+# 
+#         parentItem = self.findTargetByName(parent)
+#         i = parentItem.rowCount()
+#         while (i != 0):
+#             child = parentItem.child(i - 1)
+#             childId = child.data(Qt.UserRole + 1)
+#             if (childId == probeId):
+#                 parentItem.removeRow(i - 1)
+#                 break
+#             i -= 1
+# 
+#         newItem = ProbeItem()
+#         newItem.setEnabled(True)
+#         newItem.setSelectable(True)
+#         newItem.setData(name,   Qt.DisplayRole)
+#         newItem.setData(icon,   Qt.DecorationRole)
+#         newItem.setData(itemType, Qt.UserRole)
+#         newItem.setData(pid,    Qt.UserRole + 1)
+#         newItem.setData(parent, Qt.UserRole + 2)
+#         newItem.setData(timeout,Qt.UserRole + 3)
+#         newItem.setData(step,   Qt.UserRole + 4)
+#         newItem.setData(name,   Qt.UserRole + 5)
+#         newItem.setData(status, Qt.UserRole + 6)
+#         newItem.setFlags(Qt.ItemIsSelectable)
+#         newItem.setFlags(Qt.ItemIsEnabled)
+# 
+#         # temporaire toutes les probes id == 1 propagent leur status
+#         # au parent
+#         if (pid == 1):
+#             parentItem.setData(icon, Qt.DecorationRole)
+# 
+#         parentItem.appendRow(newItem)
+# 
+# class ProbeItem(QStandardItem):
+#     def __init__(self):
+#         super(ProbeItem, self).__init__()
+# 
+# 
+#     def data(self, role):
+#         pass
