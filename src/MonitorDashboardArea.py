60903575a154e79d78d5533ba7ad9ea1bc1d9588
ssbx <sserre.bx@gmail.com>
2013-12-01 10:23:03 +0100

    dashboard layout test

--- src/MonitorDashboardArea.py
+++ src/MonitorDashboardArea.py
@@ -1,11 +1,15 @@
 from    PySide.QtGui        import *
 from    PySide.QtCore       import *
 
-class Dash(QFrame):
+class Dash(QTabWidget):
 
     def __init__(self, parent):
         super(Dash, self).__init__(parent)
         self.stackDict  = dict()
-        grid = QGridLayout(self)
-        grid.addWidget(QLabel('helll', self), 0,0)
-        self.setLayout(grid)
+        #grid = QGridLayout(self)
+        #grid.addWidget(QLabel('helll', self), 0,0)
+        #grid.addWidget(QListView(self), 0,0)
+        self.addTab(QListView(self), 'Default')
+        self.addTab(QLabel('hell', self), 'Multiple document interface')
+        self.addTab(QLabel('hell', self), 'Mapping')
+        #self.setLayout(grid)
