ec9d6bbf6f4ed1db2dd7f11d5f2bfdb44a55eaa5
ssbx <sserre.bx@gmail.com>
2013-12-18 15:09:43 +0100

    time line background is ok

--- src/EventsTimeLine.py
+++ src/EventsTimeLine.py
@@ -10,7 +10,7 @@ class EventsView(QFrame):
         #toolTipBaseHexa = Monitor.MonitorMain.singleton.rgbDict['Base']
         # self.setStyleSheet(
             #"QFrame { border-radius: 15px; background: %s}" % toolTipBaseHexa)
-        height = 105
+        height = 108
         self.setFixedHeight(height)
         grid = QGridLayout(self)
         self.graphicsView = EventsViewGraphicScene(self, height)
@@ -22,9 +22,9 @@ class EventsViewGraphicScene(QGraphicsView):
         super(EventsViewGraphicScene, self).__init__(parent)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        height = height - 7
+        height = height - 6
 
-        self._vertiSpace    = height  / 7
+        self._vertiSpace    = height  / 6
         self._vertiStart    = -height / 2
 
         self._needRedraw = False
@@ -37,7 +37,6 @@ class EventsViewGraphicScene(QGraphicsView):
         self.warningLine    = QGraphicsLineItem(QLine(0,0,0,0))
         self.criticalLine   = QGraphicsLineItem(QLine(0,0,0,0))
         self.unknownLine    = QGraphicsLineItem(QLine(0,0,0,0))
-        self.stealthLine    = QGraphicsLineItem(QLine(0,0,0,0))
 
         backPen = QPen(QColor(Qt.lightGray))
 
@@ -46,15 +45,34 @@ class EventsViewGraphicScene(QGraphicsView):
         self.warningLine.setPen(backPen)
         self.criticalLine.setPen(backPen)
         self.unknownLine.setPen(backPen)
-        self.stealthLine.setPen(backPen)
-
 
         self.scene.addItem(self.extLine)
         self.scene.addItem(self.okLine)
         self.scene.addItem(self.warningLine)
         self.scene.addItem(self.criticalLine)
         self.scene.addItem(self.unknownLine)
-        self.scene.addItem(self.stealthLine)
+
+        self.extImage = QGraphicsSvgItem(TkorderIcons.getImage('dialog-information'))
+        self.extImage.scale(0.37,0.37)
+
+        self.okImage = QGraphicsSvgItem(TkorderIcons.getImage('weather-clear'))
+        self.okImage.scale(0.37,0.37)
+
+        self.warningImage = QGraphicsSvgItem(TkorderIcons.getImage('weather-showers'))
+        self.warningImage.scale(0.37,0.37)
+
+        self.criticalImage = QGraphicsSvgItem(TkorderIcons.getImage('weather-severe-alert'))
+        self.criticalImage.scale(0.37,0.37)
+
+        self.unknownImage = QGraphicsSvgItem(TkorderIcons.getImage('weather-clear-night'))
+        self.unknownImage.scale(0.37,0.37)
+
+        self.scene.addItem(self.extImage)
+        self.scene.addItem(self.okImage)
+        self.scene.addItem(self.warningImage)
+        self.scene.addItem(self.criticalImage)
+        self.scene.addItem(self.unknownImage)
+
 
     def _scale(self):
         size    = self.size()
@@ -63,22 +81,35 @@ class EventsViewGraphicScene(QGraphicsView):
         rightWidth  = width
         vertiSpace  = self._vertiSpace
         vertiStart  = self._vertiStart
+        vertIconPadding = 9
+        horiIconPadding = 20
 
+
+        # draw background grid
         vert = vertiStart + vertiSpace
         self.extLine.setLine(       QLine(-leftWidth, vert, rightWidth, vert))
+        self.extImage.setPos(QPointF(QPoint(-leftWidth -horiIconPadding, vert - vertIconPadding)))
+
         vert += vertiSpace
         self.okLine.setLine(        QLine(-leftWidth, vert, rightWidth, vert))
+        self.okImage.setPos(QPointF(QPoint(-leftWidth -horiIconPadding, vert - vertIconPadding)))
+
         vert += vertiSpace
         self.warningLine.setLine(   QLine(-leftWidth, vert, rightWidth, vert))
+        self.warningImage.setPos(QPointF(QPoint(-leftWidth -horiIconPadding, vert - vertIconPadding)))
+
         vert += vertiSpace
         self.criticalLine.setLine(  QLine(-leftWidth, vert, rightWidth, vert))
+        self.criticalImage.setPos(QPointF(QPoint(-leftWidth -horiIconPadding, vert - vertIconPadding)))
+
         vert += vertiSpace
         self.unknownLine.setLine(   QLine(-leftWidth, vert, rightWidth, vert))
-        vert += vertiSpace
-        self.stealthLine.setLine(   QLine(-leftWidth, vert, rightWidth, vert))
+        self.unknownImage.setPos(QPointF(QPoint(-leftWidth -horiIconPadding, vert - vertIconPadding)))
 
         self.updateSceneRect(QRectF(0,0,width,112))
         self.centerOn(0,0)
+
+        # TODO draw datas as large lines including tips on mouseover
 
 
     def resizeEvent(self, event):
