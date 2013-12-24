4b8912f59e3a8b4a9352a474a3db0eb85ef98d54
ssbx <sserre.bx@gmail.com>
2013-12-24 12:14:18 +0100

    Target overall probe view widget work

--- src/WorkingTargetView.py
+++ src/WorkingTargetView.py
@@ -53,22 +53,159 @@ class TargetHead(QFrame):
         #self.setBackgroundRole(QPalette.Window)
         #self.setAutoFillBackground(True)
         self.setFrameShape(QFrame.StyledPanel)
-        self.setFrameShadow(QFrame.Sunken)
+        self.setFrameShadow(QFrame.Raised)
 
         self.toggleButton = QPushButton('Toggle', self)
         self.toggleButton.clicked.connect(parent.toggleBody)
 
+        self.summary    = TargetProbeOverallSummary(self)
+
         self.promoteCheck = QCheckBox('include in Dashboard', self)
+
         grid = QGridLayout(self)
         grid.addWidget(self.toggleButton,    0,0)
         grid.addWidget(QLabel(target, self), 0,1)
-        grid.addWidget(self.promoteCheck,    0,3)
+        grid.addWidget(self.summary,         0,2)
+        grid.addWidget(self.promoteCheck,    0,4)
         grid.setColumnStretch(0,0)
         grid.setColumnStretch(1,0)
-        grid.setColumnStretch(2,1)
-        grid.setColumnStretch(3,0)
+        grid.setColumnStretch(2,0)
+        grid.setColumnStretch(3,1)
+        grid.setColumnStretch(4,0)
         self.setLayout(grid)
 
 class TargetBody(QFrame):
     def __init__(self, parent):
         super(TargetBody, self).__init__(parent)
+
+
+# class Window(QtGui.QWidget):
+#     def __init__(self):
+#         super(Window, self).__init__()
+# 
+#         flowLayout = FlowLayout()
+#         flowLayout.addWidget(QtGui.QPushButton("Short"))
+#         flowLayout.addWidget(QtGui.QPushButton("Longer"))
+#         flowLayout.addWidget(QtGui.QPushButton("Different text"))
+#         flowLayout.addWidget(QtGui.QPushButton("More text"))
+#         flowLayout.addWidget(QtGui.QPushButton("Even longer button text"))
+#         self.setLayout(flowLayout)
+# 
+#         self.setWindowTitle("Flow Layout")
+
+
+class TargetProbeSummaryWidget(QLabel):
+    def __init__(self, parent):
+        super(TargetProbeSummary, self).__init__(parent)
+
+class TargetProbeOverallSummary(QFrame):
+    def __init__(self, parent):
+        super(TargetProbeOverallSummary, self).__init__(parent)
+        self.setFixedWidth(600)
+        layout = FlowLayout(self)
+
+        layout.addWidget(QPushButton("Short"))
+        layout.addWidget(QPushButton("Longer"))
+        layout.addWidget(QPushButton("Different text"))
+        layout.addWidget(QPushButton("More text"))
+        layout.addWidget(QPushButton("Short"))
+        layout.addWidget(QPushButton("Short"))
+        layout.addWidget(QPushButton("Short"))
+        layout.addWidget(QPushButton("Short"))
+        layout.addWidget(QPushButton("Longer"))
+        layout.addWidget(QPushButton("Different text"))
+        layout.addWidget(QPushButton("More text"))
+        layout.addWidget(QPushButton("Longer"))
+        layout.addWidget(QPushButton("Different text"))
+        layout.addWidget(QPushButton("More text"))
+        layout.addWidget(QPushButton("Longer"))
+        layout.addWidget(QPushButton("Different text"))
+        layout.addWidget(QPushButton("More text"))
+        layout.addWidget(QPushButton("Longer"))
+        layout.addWidget(QPushButton("Different text"))
+        layout.addWidget(QPushButton("More text"))
+        self.setLayout(layout)
+
+class FlowLayout(QLayout):
+    def __init__(self, parent=None, margin=0, spacing=-1):
+        super(FlowLayout, self).__init__(parent)
+
+        #if parent is not None:
+            #self.setMargin(margin)
+
+        self.setSpacing(spacing)
+
+        self.itemList = []
+
+    def __del__(self):
+        item = self.takeAt(0)
+        while item:
+            item = self.takeAt(0)
+
+    def addItem(self, item):
+        self.itemList.append(item)
+
+    def count(self):
+        return len(self.itemList)
+
+    def itemAt(self, index):
+        if index >= 0 and index < len(self.itemList):
+            return self.itemList[index]
+
+        return None
+
+    def takeAt(self, index):
+        if index >= 0 and index < len(self.itemList):
+            return self.itemList.pop(index)
+
+        return None
+
+    def expandingDirections(self):
+        return Qt.Orientations(Qt.Orientation(0))
+
+    def hasHeightForWidth(self):
+        return True
+
+    def heightForWidth(self, width):
+        height = self.doLayout(QRect(0, 0, width, 0), True)
+        return height
+
+    def setGeometry(self, rect):
+        super(FlowLayout, self).setGeometry(rect)
+        self.doLayout(rect, False)
+
+    def sizeHint(self):
+        return self.minimumSize()
+
+    def minimumSize(self):
+        size = QSize()
+
+        for item in self.itemList:
+            size = size.expandedTo(item.minimumSize())
+
+        #size += QSize(2 * self.margin(), 2 * self.margin())
+        return size
+
+    def doLayout(self, rect, testOnly):
+        x = rect.x()
+        y = rect.y()
+        lineHeight = 0
+
+        for item in self.itemList:
+            wid = item.widget()
+            spaceX = self.spacing() + wid.style().layoutSpacing(QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Horizontal)
+            spaceY = self.spacing() + wid.style().layoutSpacing(QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Vertical)
+            nextX = x + item.sizeHint().width() + spaceX
+            if nextX - spaceX > rect.right() and lineHeight > 0:
+                x = rect.x()
+                y = y + lineHeight + spaceY
+                nextX = x + item.sizeHint().width() + spaceX
+                lineHeight = 0
+
+            if not testOnly:
+                item.setGeometry(QRect(QPoint(x, y), item.sizeHint()))
+
+            x = nextX
+            lineHeight = max(lineHeight, item.sizeHint().height())
+
+        return y + lineHeight - rect.y()
