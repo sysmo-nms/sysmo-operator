1a8927161a79d89b5b89682a55374cb2721b6c38
ssbx <sserre.bx@gmail.com>
2014-01-23 11:17:34 +0100

    custom widget work

--- src/LoggerViewEventsTimeLineSimple.py
+++ src/LoggerViewEventsTimeLineSimple.py
@@ -23,8 +23,13 @@ class SimpleTimeLine(QWidget):
 
         self.borderBrush            = QBrush(self.windowColor)
         self.timeLineUnknownBrush   = QBrush(self.darkColor)
+        
+        self.tsEnd      = None
+        self.tsStart    = None
+        self.timeNow    = None
+        self.selection  = None
 
-        self.eventDatas             = list()
+        self.eventDatas = list()
         self._initSliders()
 
     def handleProbeEvent(self, msg):
@@ -40,13 +45,12 @@ class SimpleTimeLine(QWidget):
         data            = msg['data']
         data.sort(key=operator.itemgetter('insertTs'))
         self.eventDatas = data
-        for l in self.eventDatas:
-            print l['insertTs'] / 1000000, l['status']
+        #for l in self.eventDatas:
+        #    print l['insertTs'] / 1000000, l['status']
 
     def _updateData(self, msg): pass
 
     def _updateGraph(self):
-        figureWidth     = self.size().width()
 
         secondRange     = self.timeRange
         secondFromNow   = self.stopTime
@@ -54,14 +58,21 @@ class SimpleTimeLine(QWidget):
         tsEnd           = timeNow + secondFromNow
         tsStart         = tsEnd - secondRange
 
-        #print secondRange, " ", secondFromNow, " ", figureWidth, " ", timeNow
-        print 'graph will start ad ', tsStart, ' and end at ', tsEnd
-
-        print self
-        for i in range(len(self.eventDatas)):
-            ts = self.eventDatas[i]['insertTs'] / 1000000
-            if ts > tsStart and ts < tsEnd:
-                print ts
+        self.tsEnd      = tsEnd
+        self.tsStart    = tsStart
+        self.timeNow    = timeNow
+        self.selection = list()
+        for val in reversed(self.eventDatas):
+            ts = val['insertTs'] / 1000000
+            if ts < tsEnd and ts > tsStart:
+                self.selection.append(val)
+            else:
+                # add the last past out of bound event and
+                # no need to go further.
+                self.selection.append(val)
+                break
+ 
+        self.update()
 
     # QWidget custom painting
     def sizeHint(self):
@@ -75,7 +86,8 @@ class SimpleTimeLine(QWidget):
 
     def paintEvent(self, paintEvent):
         self._drawContainer()
-        self._drawTimeLines()
+        if self.selection != None:
+            self._drawTimeLines()
 
     def _drawContainer(self):
         border  = QRect(0.0,0.0,self.widgetWidth,self.widgetHeight)
@@ -85,6 +97,47 @@ class SimpleTimeLine(QWidget):
         painter.drawRoundRect(border, 5,5)
 
     def _drawTimeLines(self):
+        figureTics      = self.tsEnd - self.tsStart
+        figurePix       = self.size().width()
+        ticsPerPix      = figureTics / figurePix
+        print "total  tics: ", figureTics
+        print "figure pix:  ", figurePix
+        print "tics per pixel: ", ticsPerPix
+
+        toDrawFromEnd   = list()
+        selLen          = len(self.selection)
+        remainingPix    = figurePix
+
+        print len(self.selection)
+
+        for i in range(len(self.selection)):
+            eventTs     = self.selection[i]['insertTs']     / 1000000
+            if i == 0: 
+                # last event
+                totalTics   = self.timeNow - eventTs
+                totalPix    = totalTics / ticsPerPix
+                #print remainingPix, totalPix, totalTics
+                remainingPix = remainingPix - totalPix
+            elif i == selLen - 1:
+                # first out of bound event, with will be the remaining
+                # available pixels
+                #print remainingPix
+                totalPix    = remainingPix
+            else:
+                nextEventTs = self.selection[i - 1]['insertTs'] / 1000000
+                totalTics   = nextEventTs - eventTs
+                totalPix    = totalTics / ticsPerPix
+                #print remainingPix
+                remainingPix = remainingPix - totalPix
+            
+            anEvent = dict()
+            anEvent['pixWidth']     = totalPix
+            anEvent['status']       = self.selection[i]['status']
+            toDrawFromEnd.append(anEvent)
+
+        #print toDrawFromEnd
+
+
         atimeLine = QRect(1,1,80,self.timeLineHeight)
         painter = QPainter(self)
         painter.setPen(self.altColor)
