b2297998ef236ee6bf26f64859e98dbe5b023904
ssbx <sserre.bx@gmail.com>
2014-02-09 21:43:51 +0100

    Work on commercial presentation

--- src/Commercials.py
+++ src/Commercials.py
@@ -32,17 +32,46 @@ class LocatorAd(QFrame):
         getLicenceButton.clicked.connect(self.showUpdateLicence)
         startTrialButton    = QPushButton('Start Trial',    self)
         printQuote          = QPushButton('Print quote',     self)
+        screenShots         = QPushButton('Screenshots',     self)
 
         centralGrid.addWidget(startTrialButton, 0,0)
         centralGrid.addWidget(getLicenceButton, 1,0)
         centralGrid.addWidget(printQuote,       2,0)
         centralGrid.addWidget(moreInfoButton,   3,0)
+        centralGrid.addWidget(screenShots,      4,0)
         
         infoLabel   = self.generateFormatedText()
-        centralGrid.addWidget(infoLabel,         0,1,4,1)
+        centralGrid.addWidget(infoLabel,         0,1,6,2)
 
-        insertKey = self.generateInsertKeyForm()
-        centralGrid.addWidget(insertKey,         4,0,1,2)
+        # separator
+        separator = QFrame(self)
+        separator.setFrameShape(QFrame.HLine)
+        separator.setFrameShadow(QFrame.Raised)
+        centralGrid.addWidget(separator,            6,0,1,3)
+
+        # insert key
+        insertKeyLabel = QLabel('Insert license key: ', self)
+        insertKeyLineEdit = QLineEdit(self)
+        insertKeyButton   = QPushButton('Activate', self)
+
+        centralGrid.addWidget(insertKeyLabel,       7,0)
+        centralGrid.addWidget(insertKeyLineEdit,    7,1)
+        centralGrid.addWidget(insertKeyButton,      7,2)
+
+        # screenshot
+        apix = QPixmap('./commercials/snapshot_tko4.png')
+        bpix = apix.scaledToHeight(200, Qt.SmoothTransformation)
+        cpix = bpix.transformed(QTransform().rotate(3))
+        scFrame = QFrame(self)
+        scGrid  = QGridLayout(scFrame)
+        scLabel1 = QLabel(self)
+        scLabel1.setPixmap(cpix)
+        scGrid.addWidget(scLabel1,  0,0)
+        scGrid.setRowStretch(0,0)
+        scGrid.setRowStretch(2,1)
+        scFrame.setLayout(scGrid)
+
+        centralGrid.addWidget(scFrame, 0,3,8,1)
         centralFrame.setLayout(centralGrid)
 
         return centralFrame
@@ -57,7 +86,28 @@ class LocatorAd(QFrame):
         return insertFrame
 
     def generateFormatedText(self):
-        return QLabel('locator descriptions et screenshots', self)
+        text = '''
+            <h1>The Locator Opus</h1>
+            <p>qsdfqsdfffffff qsdf qsdf qsdfqsdflkjqsdlfkj qsdlkj qsdflkj qdslfkj 
+            <p>qsdfqsdfffffff qsdf qsdf qsdfqsdflkjqsdlfkj qsdlkj qsdflkj qdslfkj 
+            A PySide.QtGui.QTextDocument can be edited programmatically using a PySide.QtGui.QTextCursor , and its contents can be examined by traversing the document structure. The entire document structure is stored as a hierarchy of document elements beneath the root frame, found with the PySide.QtGui.QTextDocument.rootFrame() function. Alternatively, if you just want to iterate over the textual contents of the document you can use PySide.QtGui.QTextDocument.begin() , PySide.QtGui.QTextDocument.end() , and PySide.QtGui.QTextDocument.findBlock() to retrieve text blocks that you can examine and iterate over.
+            </p>
+            <h2>Fonctionnality</h2>
+            <p>
+            A PySide.QtGui.QTextDocument can be edited programmatically using a PySide.QtGui.QTextCursor , and its contents can be examined by traversing the document structure. The entire document structure is stored as a hierarchy of document elements beneath the root frame, found with the PySide.QtGui.QTextDocument.rootFrame() function. Alternatively, if you just want to iterate over the textual contents of the document you can use PySide.QtGui.QTextDocument.begin() , PySide.QtGui.QTextDocument.end() , and PySide.QtGui.QTextDocument.findBlock() to retrieve text blocks that you can examine and iterate over.
+            </p>
+            <h2>Benefits</h2>
+            <p>
+            A PySide.QtGui.QTextDocument can be edited programmatically using a PySide.QtGui.QTextCursor , and its contents can be examined by traversing the document structure. The entire document structure is stored as a hierarchy of document elements beneath the root frame, found with the PySide.QtGui.QTextDocument.rootFrame() function. Alternatively, if you just want to iterate over the textual contents of the document you can use PySide.QtGui.QTextDocument.begin() , PySide.QtGui.QTextDocument.end() , and PySide.QtGui.QTextDocument.findBlock() to retrieve text blocks that you can examine and iterate over.
+            </p>
+        '''
+        #document = QTextDocument(self)
+        #document.setHtml(text)
+        #roEdit  = QTextEdit(self)
+        #roEdit.setDocument(document)
+        label = QLabel(text,self)
+        label.setWordWrap(True)
+        return label
 
     def showLocatorDescr(self):
         url = QUrl('http://www.qtcentre.org/threads/19616-Set-Position-of-QLabel')
