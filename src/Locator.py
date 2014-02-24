8a3ff59759f198ec079ec413e3738b238d717bf8
ssbx <sserre.bx@gmail.com>
2014-02-24 12:45:05 +0100

    New Opus locator

--- src/Locator.py
+++ src/Locator.py
@@ -1,7 +1,6 @@
 from    PySide.QtGui    import *
 from    PySide.QtCore   import *
-from    PySide.QtSvg    import *
-import  TkorderIcons
+from    LocatorProxyEvents import ChannelHandler
 import  Commercials
 
 import  TkorderMain
@@ -10,14 +9,21 @@ class LocatorMain(QFrame):
     def __init__(self, parent):
         super(LocatorMain, self).__init__(parent)
         LocatorMain.singleton = self
-        self.enabled = False
+        #self.enabled = False
+        self.eventHandler   = ChannelHandler(self)
+        self.enabled = True
         grid = self.initLocatorLayout()
         self.setLayout(grid)
 
     def initLocatorLayout(self):
-        ad = Commercials.LocatorAd(self)
-        grid = QGridLayout(self)
-        grid.addWidget(ad, 0,0)
+        if self.enabled == False:
+            ad      = Commercials.LocatorAd(self)
+            grid    = QGridLayout(self)
+            grid.addWidget(ad, 0,0)
+        elif self.enabled == True:
+            lab  = QLabel('locator', self)
+            grid = QGridLayout(self)
+            grid.addWidget(lab)
         
 
     def toggleButtonClicked(self):
---
+++ src/LocatorProxyEvents.py
@@ -0,0 +1,13 @@
+from    PySide.QtCore   import *
+import  Supercast
+
+class ChannelHandler(QObject):
+    
+    def __init__(self, parent, maxChans=3):
+        super(ChannelHandler, self).__init__(parent)
+        ChannelHandler.singleton = self
+        self.sc         = Supercast.Link.singleton
+        self.sc.setMessageProcessor('modLocatorPDU', self.handleMsg)
+        self.masterChan = 'locator-MasterChan'
+        
+    def handleMsg(self, msg): pass
