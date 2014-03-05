5f446f55e907a563ac572eb9363b027ba114a89e
ssbx <sserre.bx@gmail.com>
2014-03-05 16:55:09 +0100

    Setup.py modif

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 import py2exe
 sys.path.append("C:\\WINDOWS\\WinSxS\\x86_Microsoft.VC90.CRT_1fc8b3b9a1e18e3b_9.0.21022.8_x-ww_d08d0375")
 setup(
-    windows = ['noctopus-client.pyw'],
+    windows = ['noctopus'],
     options = {
         'py2exe' : {
             'includes' :['sys']
