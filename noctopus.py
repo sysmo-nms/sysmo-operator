35da44c1e10f7c63816d99f32d8f4cfc387ff66d
ssbx <sserre.bx@gmail.com>
2014-03-23 02:25:04 -0700

    set plastique theme as default on windows XP

--- noctopus.py
+++ noctopus.py
@@ -1,12 +1,27 @@
 # python lib
 import  sys
+import  re
+import  platform
 from PySide.QtGui       import QApplication
 import noctopus_main
 import noctopus_api
 
 
 if __name__ == '__main__':
-    noctopusApp     = QApplication(sys.argv)
-    noctopus        = noctopus_main.NMainWindow()
+    noctopusApp = QApplication(sys.argv)
+
+    osType = platform.platform()
+    if   re.match('^Windows-XP..*',    osType) != None:
+        noctopusApp.setStyle('plastique')
+    elif re.match('^Windows-Vista..*', osType) != None:
+        #noctopusApp.setStyle('plastique')
+        # test on vista
+        pass
+    elif re.match('^Windows-7..*',     osType) != None:
+        #noctopusApp.setStyle('plastique')
+        # test on 7
+        pass
+
+    noctopus    = noctopus_main.NMainWindow()
     noctopus.setWindowIcon(noctopus_api.nGetIcon('applications-development'))
     sys.exit(noctopusApp.exec_())
