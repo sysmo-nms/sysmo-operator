79e9ea84f569636b9e6e2c2bb96f8816be7189bc
ssbx <sserre.bx@gmail.com>
2014-03-24 07:26:45 -0700

    add NSplitter widget for wide margin control

--- noctopus_widgets.py
+++ noctopus_widgets.py
@@ -1,9 +1,21 @@
 # PySide
 from    PySide.QtGui    import (
     QFrame,
-    QGridLayout
+    QGridLayout,
+    QSplitter
 )
 
+# SPLITER
+class NSplitter(QSplitter):
+    def __init__(self, parent=None):
+        super(NSplitter, self).__init__(parent)
+
+class NSplitterContainer(QSplitter):
+    def __init__(self, parent=None):
+        super(NSplitterContainer, self).__init__(parent)
+        self.setContentsMargins(0,0,0,0)
+
+# FRAME
 class NFrame(QFrame):
     def __init__(self, parent=None):
         super(NFrame, self).__init__(parent)
@@ -14,7 +26,7 @@ class NFrameContainer(QFrame):
         super(NFrameContainer, self).__init__(parent)
         self.setContentsMargins(0,0,0,0)
 
-
+# GRID
 class NGrid(QGridLayout):
     def __init__(self, parent=None):
         super(NGrid, self).__init__(parent)
