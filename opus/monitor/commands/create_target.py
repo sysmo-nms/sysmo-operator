89ab3d109dc51e1ee3feb2bc353433c43680b3cd
ssbx <sserre.bx@gmail.com>
2014-04-08 12:41:18 +0200

    new form 'add Network Element'

--- opus/monitor/commands/create_target.py
+++ opus/monitor/commands/create_target.py
@@ -13,8 +13,10 @@ from PySide.QtGui   import (
     QComboBox,
     QPushButton,
     QGroupBox,
-    QListWidget
+    QListWidget,
+    QListWidgetItem
 )
+from PySide.QtCore import Qt
 
 from noctopus_widgets import NGrid, NFrame
 import nocapi
@@ -57,6 +59,7 @@ class ChooseType(QWizardPage):
         self.setSubTitle(self.tr('Choose the type of target you want to create'))
 
         typeNetElement      = QCommandLinkButton(self)
+        typeNetElement.setMaximumHeight(200)
         typeNetElement.setText('Network element')
         typeNetElement.setDescription(self.tr('''
             Using this wizard will help you to create a network element target.
@@ -68,6 +71,7 @@ class ChooseType(QWizardPage):
         typeNetElement.clicked.connect(self._startNetElement)
 
         typeNetService      = QCommandLinkButton(self)
+        typeNetService.setMaximumHeight(200)
         typeNetService.setText('Network server')
         typeNetService.setDescription(self.tr('''
             Create a network server target, and get live status of services.
@@ -114,59 +118,81 @@ class CreateNetworkElement(QWizardPage):
 
     def _initLayout(self):
         layout = NGrid(self)
-        layout.addWidget(self._initTemplates(),         0,0,1,2)
-        layout.addWidget(self._initPropertiesFrame(),   1,0,1,1)
-        layout.addWidget(self._initSnmpFrame(),         1,1,1,1)
-        layout.addWidget(self._initProbesFrame(),       2,0,1,2)
+
+        layout.addWidget(self._initMain(),         0,0)
         self.setLayout(layout)
 
-    def _initTemplates(self):
+    def _initMain(self):
         tempFrame   = NFrame(self)
         tempLayout  = NGrid(tempFrame)
+        tempLayout.setVerticalSpacing(20)
+        tempLayout.setRowStretch(1,0)
         tempFrame.setLayout(tempLayout)
-        tempLayout.addWidget(QLabel('Use a template', tempFrame),   0,0)
-        tempLayout.addWidget(QComboBox(tempFrame),                  0,1)
-        lab = QLabel('''<p>
-            The view classes that inherit PySide.QtGui.QAbstractItemView only need to implement their own view-specific functionality, such as drawing items, returning the geometry of items, finding items, etc</p>
-        ''')
-        lab.setWordWrap(True)
-        tempLayout.addWidget(lab, 1,0,1,2)
+        self._ipLine    = QLineEdit(self)
+        self._ipButton  = QComboBox(self)
+        self._ipButton.addItem('IP version 4:')
+        self._ipButton.addItem('IP version 6:')
+
+        self._snmpButton = QComboBox(self)
+        self._snmpButton.addItem('SNMP version 2b')
+        self._snmpButton.addItem('SNMP version 3')
+        self._snmpButton.addItem('Do not use SNMP')
+        self._tpButton  = QComboBox(self)
+        self._tpButton.addItem('Add a template')
+        self._templateList = self._initTempList()
+
+        tempLayout.addWidget(self._ipButton,                            0,0)
+        tempLayout.addWidget(self._ipLine,                              0,1)
+        tempLayout.addWidget(self._snmpButton,                          1,0)
+        tempLayout.addWidget(self._initSnmpFrame(),                     1,1,2,1)
+        tempLayout.addWidget(self._tpButton,                            3,0)
+        tempLayout.addWidget(self._templateList,                        3,1,2,1)
+
+        tempLayout.addWidget(QPushButton('Show details', self),         5,0)
+
+        tempLayout.setAlignment(self._snmpButton, Qt.AlignVCenter)
         tempLayout.setColumnStretch(0,0)
         tempLayout.setColumnStretch(1,1)
         return tempFrame
 
+    def _initTempList(self):
+        listW = QListWidget(self)
+        testItem = QListWidgetItem()
+        testItem.setText('Standard ICMP element')
+        testItem.setIcon(nocapi.nGetIcon('text-x-generic-template'))
+        testItem2 = QListWidgetItem()
+        testItem2.setIcon(nocapi.nGetIcon('text-x-generic-template'))
+        testItem2.setText('Standard SNMP element')
+        listW.insertItem(0,testItem)
+        listW.insertItem(1,testItem2)
+        listW.setAlternatingRowColors(True)
+        return listW
+
+
     def _initPropertiesFrame(self):
         propFrame = QGroupBox(self)
         propFrame.setTitle(self.tr("&Properties"))
-        propLayout = QFormLayout(propFrame)
+        propLayout = QGridLayout(propFrame)
+        propLayout.setColumnStretch(0,0)
+        propLayout.setColumnStretch(1,1)
         propFrame.setLayout(propLayout)
 
-        ipLabel = QLabel(self.tr("Ip address:"), propFrame)
-        ipLine  = QLineEdit(propFrame)
-
-        hnLabel = QLabel(self.tr("Hostname:"), propFrame)
-        hnLine  = QLineEdit(propFrame)
-
-        custom      = QGroupBox(propFrame)
-        custom.setTitle(self.tr("User properties"))
-        customGrid  = NGrid(custom)
-        customGrid.addWidget(QListWidget(custom),   1,0,1,2)
-        custom.setLayout(customGrid)
-
-        propLayout.insertRow(0, ipLabel, ipLine)
-        propLayout.insertRow(1, hnLabel, hnLine)
-        propLayout.insertRow(2, custom)
+        addButton = QPushButton(self)
+        addButton.setIcon(nocapi.nGetIcon('list-add'))
+        propLayout.addWidget(addButton,             0,0)
+        propLayout.addWidget(QListWidget(self),   1,0,1,2)
 
         return propFrame
 
     def _initSnmpFrame(self):
         snmpFrame = QGroupBox(self)
-        snmpFrame.setTitle(self.tr("&SNMP configuration"))
+        snmpFrame.setContentsMargins(0,0,0,0)
+        snmpFrame.setTitle(self.tr("&SNMP v2b configuration"))
         snmpLayout = QFormLayout(snmpFrame)
         snmpFrame.setLayout(snmpLayout)
 
-        snmpLayout.insertRow(0, self.tr('RO community'), QLineEdit(snmpFrame))
-        snmpLayout.insertRow(1, self.tr('RW community'), QLineEdit(snmpFrame))
+        snmpLayout.insertRow(0, self.tr('Write community:'), QLineEdit(snmpFrame))
+        snmpLayout.insertRow(1, self.tr('Read community:'), QLineEdit(snmpFrame))
         return snmpFrame
 
     def _initProbesFrame(self):
@@ -185,91 +211,6 @@ class CreateNetworkElement(QWizardPage):
         return False
 
         
-
-#         layout = QFormLayout(self)
-#         checkGroup = QButtonGroup(self)
-#         checkGroup.setExclusive(True)
-# 
-#         ipLab = QLabel(self.tr('Ip address'), self)
-#         self.ipLine = QLineEdit(self)
-#         tempLab = QLabel(self.tr('Template'), self)
-#         self.tempBox = QComboBox(self)
-#         self.tempBox.addItem('Standard SNMP')
-#         layout.insertRow(0, ipLab,      self.ipLine)
-#         layout.insertRow(1, tempLab,    self.tempBox)
-# 
-#         checkV2     = QCheckBox(self)
-#         checkV2.setText(self.tr('SNMP v2'))
-#         checkV2.clicked.connect(self._setV2Conf)
-#         checkGroup.addButton(checkV2, CreateNetworkElement.V2)
-# 
-#         layout.insertRow(2, checkV2)
-# 
-#         self.rLab    = QLabel(self.tr('Read community:'), self)
-#         self.rLine   = QLineEdit(self)
-#         self.wLab    = QLabel(self.tr('Write community:'), self)
-#         self.wLine   = QLineEdit(self)
-#         layout.insertRow(3, self.rLab, self.rLine)
-#         layout.insertRow(4, self.wLab, self.wLine)
-# 
-#         checkV3     = QCheckBox(self)
-#         checkV3.setText(self.tr('SNMP v3'))
-#         checkV3.clicked.connect(self._setV3Conf)
-#         checkGroup.addButton(checkV3, CreateNetworkElement.V3)
-# 
-#         layout.insertRow(5, checkV3)
-# 
-#         self.lab1 = QLabel('lkj', self)
-#         self.lab2 = QLabel('lkj', self)
-#         self.lab3 = QLabel('lkj', self)
-#         self.line1 = QLineEdit(self)
-#         self.line2 = QLineEdit(self)
-#         self.line3 = QLineEdit(self)
-#         layout.insertRow(6, self.lab1, self.line1)
-#         layout.insertRow(7, self.lab2, self.line2)
-#         layout.insertRow(8, self.lab3, self.line3)
-# 
-#         self.setLayout(layout)
-#         self._allDisabled()
-#         self.setFinalPage(True)
-#         self.setCommitPage(True)
-# 
-#     def _allDisabled(self):
-#         self.lab1.setDisabled(True)
-#         self.lab2.setDisabled(True)
-#         self.lab3.setDisabled(True)
-#         self.line1.setDisabled(True)
-#         self.line2.setDisabled(True)
-#         self.line3.setDisabled(True)
-#         self.rLab.setDisabled(True)
-#         self.wLab.setDisabled(True)
-#         self.rLine.setDisabled(True)
-#         self.wLine.setDisabled(True)
-# 
-#     def _setV2Conf(self):
-#         self.lab1.setDisabled(True)
-#         self.lab2.setDisabled(True)
-#         self.lab3.setDisabled(True)
-#         self.line1.setDisabled(True)
-#         self.line2.setDisabled(True)
-#         self.line3.setDisabled(True)
-#         self.rLab.setDisabled(False)
-#         self.wLab.setDisabled(False)
-#         self.rLine.setDisabled(False)
-#         self.wLine.setDisabled(False)
-# 
-#     def _setV3Conf(self):
-#         self.lab1.setDisabled(False)
-#         self.lab2.setDisabled(False)
-#         self.lab3.setDisabled(False)
-#         self.line1.setDisabled(False)
-#         self.line2.setDisabled(False)
-#         self.line3.setDisabled(False)
-#         self.rLab.setDisabled(True)
-#         self.wLab.setDisabled(True)
-#         self.rLine.setDisabled(True)
-#         self.wLine.setDisabled(True)
-# 
 
 
 class CreateNetworkServer(QWizardPage):
