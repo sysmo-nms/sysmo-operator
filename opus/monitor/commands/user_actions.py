f49d37e63ad64a6ad092454c47fadc07d11e1d5c
ssbx <sserre.bx@gmail.com>
2014-05-20 14:51:53 +0200

    user actions work

--- opus/monitor/commands/user_actions.py
+++ opus/monitor/commands/user_actions.py
@@ -4,15 +4,21 @@ from PySide.QtCore import (
 )
 from PySide.QtGui import (
     QDialog,
-    QLabel
+    QLabel,
+    QListWidget
 )
 from noctopus_widgets import (
     NGrid,
     NGridContainer
 )
 
-# TODO possibilite de declancher un evenement lors de l'execution d'une
-# action. Sera logguee sur le serveur.
+# TODO possibilite de:
+# - declancher un evenement lors de l'execution d'une action.
+# Sera logguee sur le serveur.
+# ET/OU
+# - declancher une action serveur via api locale, pour executer une commande
+# serveur. Exemple, ouverture d'un terminal [...] fermeture du terminal
+# et sauvegarde eventuelle de configuration sur un element (action serveur)
 class UserActions(QObject):
     def __init__(self, parent=None):
         super(UserActions, self).__init__(parent)
@@ -27,5 +33,14 @@ class UserActionsUI(QDialog):
     def __init__(self, parent=None):
         super(UserActionsUI, self).__init__(parent)
         grid = NGrid(self)
-        grid.addWidget(QLabel('hello user action', self), 0,0)
+        lab = QLabel('User actions', self)
+        targetList      = QListWidget(self)
+        selectedTargets = QListWidget(self)
+        selectedActions = QListWidget(self)
+        actionsList     = QListWidget(self)
+        grid.addWidget(lab, 0,0,1,4)
+        grid.addWidget(targetList,   1,0)
+        grid.addWidget(selectedTargets, 1,1)
+        grid.addWidget(selectedActions, 1,3)
+        grid.addWidget(actionsList,  1,2)
         self.setLayout(grid)
