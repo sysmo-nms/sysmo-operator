0dcaa328c21085aeda763184d7e9233847ff2968
ssbx <sserre.bx@gmail.com>
2014-07-03 18:35:46 +0200

    work on documentation engine config

--- opus/monitor/commands/doc_engine.py
+++ opus/monitor/commands/doc_engine.py
@@ -6,18 +6,38 @@ from PySide.QtCore import (
 from PySide.QtGui import (
     QDialog,
     QDialogButtonBox,
+    QPushButton,
+    QFormLayout,
     QLabel
 )
 
 from noctopus_widgets import (
     NGrid,
+    NFrameContainer,
     NGridContainer
 )
 
+import nocapi
 class DocConfigurator(QDialog):
     def __init__(self, parent=None):
         super(DocConfigurator, self).__init__(parent)
-        grid = NGrid(self)
         dialogButtons = QDialogButtonBox(self)
-        grid.addWidget(dialogButtons,   1,0)
+        save = QPushButton('Save', self)
+        save.setIcon(nocapi.nGetIcon('document-save'))
+        dialogButtons.addButton(save,    QDialogButtonBox.AcceptRole)
+        cancel = QPushButton('Cancel', self)
+        cancel.setIcon(nocapi.nGetIcon('process-stop'))
+        dialogButtons.addButton(cancel,  QDialogButtonBox.RejectRole)
+
+        config      = NFrameContainer(self)
+        configGrid  = NGrid(self)
+        config.setLayout(configGrid)
+
+        grid = NGrid(self)
+        grid.addWidget(QLabel('config', self),  0,0)
+        grid.addWidget(dialogButtons,           1,0)
+        grid.setRowStretch(0,1)
+        grid.setRowStretch(1,0)
         self.setLayout(grid)
+
+        self.exec_()
