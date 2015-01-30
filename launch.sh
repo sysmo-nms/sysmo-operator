e78e07825667ce949bbc33fcaf257004e73acdc4
ssbx <sserre.bx@gmail.com>
2015-01-30 09:58:22 +0100

    include QT path in lauch.sh

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-export QT_QPA_PLATFORM_PLUGIN_PATH=C:\Python34\Lib\site-packages\PyQt5\plugins\platforms
+export QT_QPA_PLATFORM_PLUGIN_PATH="C:\Python34\Lib\site-packages\PyQt5\plugins\platforms"
 
 /cygdrive/c/Python34/python.exe ./noctopus.py
