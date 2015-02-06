871d7818012b4cbbe818715676fcc826beef1715
ssbx <sserre.bx@gmail.com>
2015-02-06 11:22:34 +0100

    include conf box

--- opus/monitor/central/tree/toolbox.py
+++ opus/monitor/central/tree/toolbox.py
@@ -2,13 +2,23 @@ from    PyQt5.QtWidgets    import (
     QToolBox,
     QDialog,
     QWidget,
-    QLabel
+    QLabel,
+    QFrame,
+    QGridLayout,
+    QListView,
+    QListWidget,
+    QListWidgetItem,
+    QAbstractItemView,
+    QStackedWidget,
+    QPushButton
 )
 
-from    PyQt5.QtCore   import QSettings
+from    PyQt5.QtCore   import QSettings, QSize, Qt
 from    noctopus_widgets    import (
     NFrameContainer,
     NGridContainer,
+    NFrame,
+    NGrid,
     QLabel
 )
 
@@ -29,10 +39,144 @@ def openProbePropertiesFor(target):
     else:
         ProbeProperties.Elements[target].show()
         
+
 class TargetProperties(QDialog):
     Elements = dict()
     def __init__(self, element, parent = None):
-        super(TargetProperties, self).__init__()
+        super(TargetProperties, self).__init__(parent)
+        
+        self.setModal(True)
+        self.setMinimumWidth(600)
+        self.setMinimumHeight(450)
+        layout = QGridLayout(self)
+        self._confList = QListWidget(self);
+        self._confList.setViewMode(QListView.IconMode)
+        self._confList.setIconSize(QSize(70,70))
+        self._confList.setMovement(QListView.Static)
+        self._confList.setMaximumWidth(120)
+        self._confList.setSpacing(12)
+        self._confList.setVerticalScrollMode(QAbstractItemView.ScrollPerPixel)
+
+        #p1 = TargetPropertiesP1(self)
+        p1 = TargetPropertiesP1(self)
+        p2 = TargetPropertiesP2(self)
+        p3 = TargetPropertiesP3(self)
+        p4 = TargetPropertiesP4(self)
+        p5 = TargetPropertiesP5(self)
+        p6 = TargetPropertiesP6(self)
+
+        self._stack = QStackedWidget(self)
+        self._stack.addWidget(p1)
+        self._stack.addWidget(p2)
+        self._stack.addWidget(p3)
+        self._stack.addWidget(p4)
+        self._stack.addWidget(p5)
+        self._stack.addWidget(p6)
+    
+        close = QPushButton('Close', self)
+        close.clicked.connect(self.close)
+        
+    
+        layout.addWidget(self._confList, 0,0)
+        layout.addWidget(self._stack, 0,1,1,2)
+        layout.addWidget(close, 1,2)
+        layout.setColumnStretch(0,0)
+        layout.setColumnStretch(1,1)
+        layout.setColumnStretch(2,0)
+        
+        self.createIcons()
+        self.show()
+
+    def createIcons(self):
+        confButton = QListWidgetItem(self._confList)
+        confButton.setIcon(nocapi.nGetIcon('preferences-system'))
+        confButton.setText('Properties')
+        confButton.setTextAlignment(Qt.AlignHCenter)
+        confButton.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+
+        probesButton = QListWidgetItem(self._confList)
+        probesButton.setIcon(nocapi.nGetIcon('satellite'))
+        probesButton.setText('Probes')
+        probesButton.setTextAlignment(Qt.AlignHCenter)
+        probesButton.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+
+        tieButton = QListWidgetItem(self._confList)
+        tieButton.setIcon(nocapi.nGetIcon('utilities-terminal'))
+        tieButton.setText('Operator\nAction')
+        tieButton.setTextAlignment(Qt.AlignHCenter)
+        tieButton.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+
+        locationButton = QListWidgetItem(self._confList)
+        locationButton.setIcon(nocapi.nGetIcon('internet-web-browser'))
+        locationButton.setText('Location')
+        locationButton.setTextAlignment(Qt.AlignHCenter)
+        locationButton.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+
+
+        jobsButton = QListWidgetItem(self._confList)
+        jobsButton.setIcon(nocapi.nGetIcon('appointment-new'))
+        jobsButton.setText('Jobs')
+        jobsButton.setTextAlignment(Qt.AlignHCenter)
+        jobsButton.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+        jobsButton.setFlags(Qt.NoItemFlags)
+
+        tieButton = QListWidgetItem(self._confList)
+        tieButton.setIcon(nocapi.nGetIcon('applications-system'))
+        tieButton.setText('Tie scripts')
+        tieButton.setTextAlignment(Qt.AlignHCenter)
+        tieButton.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)
+        tieButton.setFlags(Qt.NoItemFlags)
+
+        self._confList.currentItemChanged.connect(self._changePage)
+
+    def _changePage(self, current, previous):
+        if current == None: 
+            current = previous;
+        self._stack.setCurrentIndex(self._confList.row(current))
+    
+
+
+        
+class TargetPropertiesP1(NFrame):
+    def __init__(self, element, parent = None):
+        super(TargetPropertiesP1, self).__init__(parent)
+        layout = QGridLayout(self)
+        layout.addWidget(QLabel('p1', self))
+
+class TargetPropertiesP2(NFrame):
+    def __init__(self, element, parent = None):
+        super(TargetPropertiesP2, self).__init__(parent)
+        layout = QGridLayout(self)
+        layout.addWidget(QLabel('p2', self))
+
+class TargetPropertiesP3(NFrame):
+    def __init__(self, element, parent = None):
+        super(TargetPropertiesP3, self).__init__(parent)
+        layout = QGridLayout(self)
+        layout.addWidget(QLabel('p3', self))
+
+class TargetPropertiesP4(NFrame):
+    def __init__(self, element, parent = None):
+        super(TargetPropertiesP4, self).__init__(parent)
+        layout = QGridLayout(self)
+        layout.addWidget(QLabel('p4', self))
+
+class TargetPropertiesP5(NFrame):
+    def __init__(self, element, parent = None):
+        super(TargetPropertiesP5, self).__init__(parent)
+        layout = QGridLayout(self)
+        layout.addWidget(QLabel('p5', self))
+
+class TargetPropertiesP6(NFrame):
+    def __init__(self, element, parent = None):
+        super(TargetPropertiesP6, self).__init__(parent)
+        layout = QGridLayout(self)
+        layout.addWidget(QLabel('p6', self))
+
+class TargetPropertiesX(QDialog):
+    Elements = dict()
+    def __init__(self, element, parent = None):
+        super(TargetPropertiesX, self).__init__()
         nocapi.nConnectWillClose(self._willClose)
         self._element = element
         self.setWindowTitle(element)
