1a200cb65a02cb53819c456a77e8dd0821f83f94
ssbx <sserre.bx@gmail.com>
2015-03-20 17:16:56 +0100

    add configurable theme to QMenu and QMenuBar

--- sysmo.py
+++ sysmo.py
@@ -4,7 +4,7 @@ else:
     platformPluginsDir = os.getcwd() + '\plugins\platforms'
     os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = platformPluginsDir
 
-from    PyQt5.QtWidgets import QApplication
+from    PyQt5.QtWidgets import QApplication, QWidget
 from    PyQt5.QtCore    import QSettings, QTranslator
 import  sys
 import  sysmo_main
@@ -13,15 +13,23 @@ import  sysmo_colors
 DEFAULT_PALETTE = 'dark'
 
 QApplication.setStyle('fusion')
+defaultPalette = QApplication.palette()
 sysmoSettings   = QSettings("Sysmo Monitor", "sysmo-operator")
 sysmoTheme      = sysmoSettings.value("NMainWindow/theme")
+sysmoMenuTheme  = sysmoSettings.value("NMainWindow/menuTheme")
+sysmoMenuBarTheme = sysmoSettings.value("NMainWindow/menuBarTheme")
 
-if (sysmoTheme == None):
-   sysmoTheme = DEFAULT_PALETTE
 
-if (sysmoTheme != 'native'):
+
+if (sysmoTheme != None and sysmoTheme != 'native'):
     colorPalette = sysmo_colors.getPalette(sysmoTheme)
     QApplication.setPalette(colorPalette)
+
+if (sysmoMenuBarTheme != None and sysmoMenuBarTheme != 'native'):
+    QApplication.setPalette(sysmo_colors.getPalette(sysmoMenuBarTheme), "QMenuBar")
+
+if (sysmoMenuTheme != None and sysmoMenuTheme != 'native'):
+    QApplication.setPalette(sysmo_colors.getPalette(sysmoMenuTheme), "QMenu")
 
 translator   = QTranslator()
 translator.load('fr_FR')
