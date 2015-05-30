c561f905d729304446acd292c2e806546851cc60
ssbx <sserre.bx@gmail.com>
2015-05-30 23:44:42 +0200

    some test with qpalette

--- main.cpp
+++ main.cpp
@@ -10,12 +10,15 @@ int main(int argc, char *argv[])
     // the reverse do not keep the original palette
     /*
     QApplication::setStyle(QString("fusion"));
+    QApplication::setPalete(customPalete);
     QApplication a(argc, argv);
     */
 
-    // this keep the original palette
+    /* this keep the original palette and set fusion
+     * but is not sure */
     QApplication a(argc, argv);
     QApplication::setStyle(QString("fusion"));
+
     MainWindow w;
     w.show();
 
