63b927144abc3936e0cdb9e92fb09c8001f702a3
ssbx <sserre.bx@gmail.com>
2015-05-30 23:42:23 +0200

    QPalette tests

--- main.cpp
+++ main.cpp
@@ -2,11 +2,20 @@
 
 #include <QApplication>
 #include <QString>
+#include <QPalette>
 
 int main(int argc, char *argv[])
 {
+
+    // the reverse do not keep the original palette
+    /*
     QApplication::setStyle(QString("fusion"));
     QApplication a(argc, argv);
+    */
+
+    // this keep the original palette
+    QApplication a(argc, argv);
+    QApplication::setStyle(QString("fusion"));
     MainWindow w;
     w.show();
 
