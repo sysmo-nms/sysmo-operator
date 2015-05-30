b65f5d986d3e4c868d6a1cae0bd298a79b5c9399
ssbx <sserre.bx@gmail.com>
2015-05-30 23:37:09 +0200

    force fusion theme

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QString>
 
 int main(int argc, char *argv[])
 {
+    QApplication::setStyle(QString("fusion"));
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
