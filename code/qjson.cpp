c22577e63030c208de2396be348c49c2b772201d
Sébastien Serre <sserre.bx@gmail.com>
2016-12-31 13:28:25 +0100

    update headers in source files

--- code/qjson.cpp
+++ code/qjson.cpp
@@ -1,5 +1,20 @@
 /*
-from http://codereview.stackexchange.com/questions/11927/yet-another-json-parser-and-serializer-for-qt-but-with-additional-features
+Sysmo NMS Network Management and Monitoring solution (http://www.sysmo.io)
+
+Copyright (c) 2012-2017 Sebastien Serre <ssbx@sysmo.io>
+
+Sysmo NMS is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Sysmo NMS is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Sysmo.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "qjson.h"
 #include <QStringList>
